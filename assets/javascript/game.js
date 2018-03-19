89a2e269d55729549befd99a8b4bfd07e20d8708
Sunny <sundeepkmanik@gmail.com>
2018-03-18 21:06:26 -0700

    basic onclick events added

---
+++ assets/javascript/game.js
@@ -0,0 +1,97 @@
+$(document).ready(function() {
+
+// ================== GLOBAL VARIABLES
+// ===================================
+
+// dino objects
+var julioDino = {
+    id: "yellow",
+    healthPoints: 100,
+	attackPower: 10,
+    counterAttack: 5,
+    isChosen: false,
+    isActiveEnemy: false
+};
+
+var diegoDino = {
+    id: "orange",
+    healthPoints: 80,
+    attackPower: 30,
+    counterAttack: 3,
+    isChosen: false,
+    isActiveEnemy: false
+};
+
+var andresDino = {
+    id: "purple",
+    healthPoints: 120,
+    attackPower: 5,
+    counterAttack: 10,
+    isChosen: false,
+    isActiveEnemy: false
+}
+
+// array for objects
+var dinoArray = [julioDino, diegoDino, andresDino];
+
+// function that loops through objects
+function getDinoWithId(matchId) {
+    for (var i = 0; i < dinoArray.length; i++) {
+        if (dinoArray[i].id === matchId) {
+
+            return dinoArray[i];
+
+        }
+    }
+}
+
+// ======================== GAME START
+// ===================================
+
+// onclick that starts the game, ONLY PERFORMED ONCE
+$(".dino-select").one("click", ".default-port", function() {
+
+    console.log("Selected a dino!");
+
+    // match HTML id to object id
+    var chosenId = $(this).attr("id");
+    var ourDino = getDinoWithId(chosenId);
+
+    ourDino.isChosen = true;
+
+    $(".dino-select h2").text("Player");
+    console.log(ourDino);
+
+    // remove unchosen dinos and add to enemy section
+    for (var i = 0; i < dinoArray.length; i++) {
+        if (dinoArray[i].isChosen === false) {
+
+            var unchosenDino = dinoArray[i].id;
+
+            $("#" + unchosenDino).detach().appendTo(".enemies");
+
+        }
+    }
+
+});
+
+// onclick that selects an enemy to fight, ONLY PERFORMED ONCE
+$(".enemies").one("click", ".default-port", function() {
+
+    console.log("Chosen an enemy!");
+
+    // match HTML id to object id
+    var enemyId = $(this).attr("id");
+    var enemyDino = getDinoWithId(enemyId);
+
+    enemyDino.isActiveEnemy = true;
+    console.log(enemyDino);
+
+    // move selected enemy to defender section
+    $("#" + enemyId).detach().appendTo(".defense-area");
+
+});
+
+
+// document ready closing tag
+});
